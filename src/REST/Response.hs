723c2c141dff75a717c932ba3ca4b318c49c51a1
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-06 00:49:33 +0900

    add JSON interfaces for REST API.

--- src/REST/Response.hs
+++ src/REST/Response.hs
@@ -3,6 +3,8 @@
 module REST.Response (
   Hello (..),
   StatusSnapshot (..), Snapshot (..),
+  ProblemSubmission (..),
+  SolutionSubmission (..),
   ) where
 
 import GHC.Generics (Generic)
@@ -15,10 +17,12 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
 
 
+type Timestamp = Word64
 
 stripPrefix' :: Eq a => [a] -> [a] -> [a]
 stripPrefix' pre s =
   fromMaybe s $ stripPrefix pre s
+
 
 data Hello =
   Hello
@@ -46,7 +50,7 @@ _t_hello =
 
 data Snapshot =
   Snapshot
-  { snapshot_time  ::  Word64
+  { snapshot_time  ::  Timestamp
   , snapshot_hash  ::  String
   } deriving (Eq, Show, Generic)
 
@@ -83,4 +87,90 @@ _t_statusSnapshot =
      [ Snapshot 1470355200 "0123456789abcdef0123456789abcdef"
      , Snapshot 1470358800 "fedcba9876543210fedcba9876543210" ]},
    Aeson.decode $
-   L8.pack "{\"ok\":true,\"snapshots\":[{\"snapshot_time\":1470355200,\"snapshot_hash\":\"0123456789abcdef0123456789abcdef\"},{\"snapshot_time\":1470358800,\"snapshot_hash\":\"fedcba9876543210fedcba9876543210\"}]}")
+   L8.pack $ concat
+   [ "{"
+   , "  \"ok\": true,"
+   , "  \"snapshots\": ["
+   , "    {"
+   , "      \"snapshot_time\": 1470355200,"
+   , "      \"snapshot_hash\": \"0123456789abcdef0123456789abcdef\""
+   , "    },"
+   , "    {"
+   , "      \"snapshot_time\": 1470358800,"
+   , "      \"snapshot_hash\": \"fedcba9876543210fedcba9876543210\""
+   , "    }"
+   , "  ]"
+   , "}"
+   ])
+
+
+data ProblemSubmission =
+  ProblemSubmission
+  { problemSubmission_ok                  ::  Bool
+  , problemSubmission_problem_id          ::  Int
+  , problemSubmission_publish_time        ::  Timestamp
+  , problemSubmission_solution_spec_hash  ::  String
+  , problemSubmission_solution_size       ::  Int
+  , problemSubmission_problem_spec_hash   ::  String
+  , problemSubmission_problem_size        ::  Int
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON ProblemSubmission where
+  toJSON =
+    genericToJSON
+    Aeson.defaultOptions
+    { Aeson.fieldLabelModifier = stripPrefix' "problemSubmission_" }
+
+instance FromJSON ProblemSubmission where
+  parseJSON =
+    genericParseJSON
+    Aeson.defaultOptions
+    { Aeson.fieldLabelModifier = stripPrefix' "problemSubmission_" }
+
+_t_problemSubmission :: Maybe ProblemSubmission
+_t_problemSubmission =
+  Aeson.decode . L8.pack $ concat
+  [ "{"
+  , "  \"ok\": true,"
+  , "  \"problem_id\": 4,"
+  , "  \"publish_time\": 1475280000,"
+  , "  \"solution_spec_hash\": \"27e3c42fa46aec6fcf438bb5c326d55e27c91811\","
+  , "  \"solution_size\": 78,"
+  , "  \"problem_spec_hash\": \"f4b1a8567108144bae331340a57c68b85df487e0\","
+  , "  \"problem_size\": 67"
+  , "}"
+  ]
+
+
+data SolutionSubmission =
+  SolutionSubmission
+  { solutionSubmission_ok                  ::  Bool
+  , solutionSubmission_problem_id          ::  Int
+  , solutionSubmission_resemblance         ::  Double
+  , solutionSubmission_solution_spec_hash  ::  String
+  , solutionSubmission_solution_size       ::  Int
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON SolutionSubmission where
+  toJSON =
+    genericToJSON
+    Aeson.defaultOptions
+    { Aeson.fieldLabelModifier = stripPrefix' "solutionSubmission_" }
+
+instance FromJSON SolutionSubmission where
+  parseJSON =
+    genericParseJSON
+    Aeson.defaultOptions
+    { Aeson.fieldLabelModifier = stripPrefix' "solutionSubmission_" }
+
+_t_solutionSubmission :: Maybe SolutionSubmission
+_t_solutionSubmission =
+  Aeson.decode . L8.pack $ concat
+  [ "{"
+  , "  \"ok\": true,"
+  , "  \"problem_id\": 1,"
+  , "  \"resemblance\": 1.0,"
+  , "  \"solution_spec_hash\": \"27e3c42fa46aec6fcf438bb5c326d55e27c91811\","
+  , "  \"solution_size\": 78"
+  , "}"
+  ]
