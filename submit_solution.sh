7bc59f55e32db5a08d10d8414569fa563978599a
cutsea110 <cutsea110@gmail.com>
2016-08-05 18:21:09 +0900

    usable

--- submit_solution.sh
+++ submit_solution.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ $# -eq 1 ]; then
  curl --compressed -L -H Expect: -H "X-API-Key: 49-99eab0ca16efde61012b3a535bab0edb" -F "problem_id=$1" -F "solution_spec=@answers/$1.dat" "http://2016sv.icfpcontest.org/api/solution/submit" > responses/$1.json
+ cat responses/$1.json
 else
  echo "$0 <qno>"
 fi
