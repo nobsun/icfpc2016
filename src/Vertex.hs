50a90069e9058a28830edee9813c617130ca8357
Nobuo Yamashita <nobsun@sampou.org>
2016-08-05 13:24:31 +0900

    fix name

--- src/Vertex.hs
+++ src/Vertex.hs
@@ -1,4 +1,4 @@
-module Point where
+module Vertex where
 
 import Data.Char
 import Data.Bool
@@ -6,32 +6,34 @@ import Data.Array
 import Data.Ratio
 import Text.ParserCombinators.ReadP
 
-data Point = Point { xcoord, ycoord :: Rational }
+data Vertex = Vertex { xcoord, ycoord :: Rational }
 
-instance Show Point where
+instance Show Vertex where
   show p = showRational (xcoord p) ++ "," ++ showRational (ycoord p)
 
 showRational :: Rational -> String
 showRational r = show (numerator r) ++ bool "" ('/':show d) (1 /= d)
   where d = denominator r
 
-instance Read Point where
-  readsPrec _ = readP_to_S parsePoint
+instance Read Vertex where
+  readsPrec _ = readP_to_S parseVertex
 
-parsePoint :: ReadP Point
-parsePoint = do { x <- parseRational
-                ; char ','
-                ; y <- parseRational
-                ; return (Point x y)
-                }
+parseVertex :: ReadP Vertex
+parseVertex = do
+  { x <- parseRational
+  ; char ','
+  ; y <- parseRational
+  ; return (Vertex x y)
+  }
 
 parseRational :: ReadP Rational
-parseRational = do { nd <- sepBy1 parseInteger (char '/')
-                   ; return $ case nd of
-                       [n]   -> n % 1
-                       [n,d] -> n % d
-                   }
+parseRational = skipSpaces >> do
+  { nd <- sepBy1 parseInteger (char '/')
+  ; return $ case nd of
+               [n]   -> n % 1
+               [n,d] -> n % d
+  }
 
 parseInteger :: ReadP Integer
-parseInteger = readS_to_P (Prelude.readsPrec 0)
+parseInteger = skipSpaces >> readS_to_P (Prelude.readsPrec 0)
 
