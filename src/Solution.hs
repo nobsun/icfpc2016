5d082dcb9c7ad80f62cd0c95e2da5f70c8446997
Nobuo Yamashita <nobsun@sampou.org>
2016-08-05 14:15:01 +0900

    define Show and Read instances for Solution

--- src/Solution.hs
+++ src/Solution.hs
@@ -2,11 +2,68 @@ module Solution where
 
 import Text.ParserCombinators.ReadP
 import Vertex
+import Problem
 
 data Solution = Solution
-  { nVertex   :: Int
-  , vertices  :: [(Int,Vertex)]
-  , nFacet    :: Int
-  , facets    :: [(Int,[Int])]
-  , moves     :: [(Int,Int)]
+  { snVertex  :: Int
+  , svertice  :: [(Int,Vertex)]
+  , nFacet   :: Int
+  , facets   :: [Facet]
+  , moves    :: [Vertex]
   }
+
+type Facet = (Int, [Int])
+
+instance Show Solution where
+  show s = unlines
+         $ show (snVertex s)
+         : (map (show . snd) (svertice s)
+           ++ (show (nFacet s) : (map showFacet (facets s)
+                                  ++ (map show (moves s)))))
+
+showFacet :: Facet -> String
+showFacet (n,vs) = unwords (map show (n:vs))
+
+instance Read Solution where
+  readsPrec _ = readP_to_S parseSolution
+
+parseSolution :: ReadP Solution
+parseSolution = do
+  { nv <- parseInt
+  ; vs <- count nv parseVertex
+  ; nf <- parseInt
+  ; fs <- count nf parseFacet
+  ; ms <- count nv parseVertex
+  ; return (Solution nv (numbering 0 vs) nf fs ms)
+  }
+
+parseFacet :: ReadP Facet
+parseFacet = do
+  { nv <- parseInt
+  ; vs <- count nv parseInt
+  ; return (nv,vs)
+  }
+
+sample :: String
+sample = unlines
+  ["7"
+  ,"0,0"
+  ,"1,0"
+  ,"1,1"
+  ,"0,1"
+  ,"0,1/2"
+  ,"1/2,1/2"
+  ,"1/2,1"
+  ,"4"
+  ,"4 0 1 5 4"
+  ,"4 1 2 6 5"
+  ,"3 4 5 3"
+  ,"3 5 6 3"
+  ,"0,0"
+  ,"1,0"
+  ,"0,0"
+  ,"0,0"
+  ,"0,1/2"
+  ,"1/2,1/2"
+  ,"0,1/2"
+  ]
