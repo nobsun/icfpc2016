ff1d8cf00c4ed775240de1d5917d3e78a036031f
Nobuo Yamashita <nobsun@sampou.org>
2016-08-05 11:38:19 +0900

    modify showRational

--- src/Point.hs
+++ src/Point.hs
@@ -9,7 +9,11 @@ import Text.ParserCombinators.ReadP
 data Point = Point { xcoord, ycoord :: Rational }
 
 instance Show Point where
-  show p = show (xcoord p, ycoord p)
+  show p = showRational (xcoord p) ++ "," ++ showRational (ycoord p)
+
+showRational :: Rational -> String
+showRational r = show (numerator r) ++ bool "" ('/':show d) (1 /= d)
+  where d = denominator r
 
 instance Read Point where
   readsPrec _ = readP_to_S parsePoint
