4dff4a2e10f34cb35003e5d8b00ea6bc0429e294
Nobuo Yamashita <nobsun@sampou.org>
2016-08-05 19:08:26 +0900

    move check to Valid.hs

--- src/Solution.hs
+++ src/Solution.hs
@@ -5,6 +5,7 @@ import Data.List (group,sort)
 import Data.Function (on)
 import Text.ParserCombinators.ReadP
 import Vertex
+import Segment
 import Polygon
 import Problem
 
@@ -47,59 +48,6 @@ parseFacet = do
   ; vs <- count nv parseInt
   ; return (nv,vs)
   }
-
-check :: Solution -> [Bool]
-check s = map ($ s) conds
-
-type Condition = Solution -> Bool
-
-ncond :: Int
-ncond = 3
-
-conds :: [Condition]
-conds = map cond [1..ncond]
-
-cond :: Int -> Condition
-cond 1 s = all inRange (map snd (svertice s))
-cond 2 s = uniqOccurrence (map snd (svertice s))
-cond 3 s = all (simple (svertice s)) (facets s)
-
-inRange :: Vertex -> Bool
-inRange v = 0 <= x && x <= 1 && 0 <= y && y <= 1
-  where
-    x = xcoord v
-    y = ycoord v
-
-uniqOccurrence :: [Vertex] -> Bool
-uniqOccurrence vs = length vs == length (group (sort vs))
-
-simple :: [(Int,Vertex)] -> Facet -> Bool
-simple tbl (n,ids) = all (not . segIntersection) $ comb 2 es
-  where
-    es = take n (zip vs (tail vs))
-    vs = cycle $ map lookingup ids
-    lookingup i = maybe undefined id (lookup i tbl)
-
-segIntersection :: [Segment] -> Bool
-segIntersection [(a,b),(c,d)] = tc * td < 0 && ta * tb < 0
-  where
-    ax = xcoord a
-    ay = ycoord a
-    bx = xcoord b
-    by = ycoord b
-    cx = xcoord c
-    cy = ycoord c
-    dx = xcoord d
-    dy = ycoord d
-    ta = (cx - dx) * (ay - cy) + (cy - dy) * (cx - ax)
-    tb = (cx - dx) * (by - cy) + (cy - dy) * (cx - bx)
-    tc = (ax - bx) * (cy - ay) + (ay - by) * (ax - cx)
-    td = (ax - bx) * (dy - ay) + (ay - by) * (ax - dx)
-
-comb :: Int -> [a] -> [[a]]
-comb 0 _      = [[]]
-comb _ []     = []
-comb n (x:xs) = map (x:) (comb (n-1) xs) ++ comb n xs
 
 sampleSolution :: String
 sampleSolution = unlines
