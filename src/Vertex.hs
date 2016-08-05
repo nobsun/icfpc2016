5e8e9279a9970e6c3a58f730d2a8d5d09419e0dd
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-06 08:22:41 +0900

    move Vec and moveVert.

--- src/Vertex.hs
+++ src/Vertex.hs
@@ -42,3 +42,9 @@ distance = sqrt . fromRational . sqDistance
 
 sqDistance :: (Vertex, Vertex) -> Rational
 sqDistance (v1, v2) = (xcoord v1 - xcoord v2)^2 + (ycoord v1 - ycoord v2)^2
+
+
+type Vec = (Rational,Rational)
+
+moveVert :: Vec -> Vertex -> Vertex
+moveVert (dx,dy) (Vertex x y) = Vertex (x+dx) (y+dy)
