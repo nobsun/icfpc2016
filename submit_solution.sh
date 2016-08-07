f4c46858f246d036e91fa7dd90a121e38f79f977
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 22:40:24 +0900

    fix to remove empty result.

--- submit_solution.sh
+++ submit_solution.sh
@@ -4,10 +4,15 @@ if [ $# -eq 2 ]; then
 fi
 
 if [ $# -ge 1 ]; then
- curl --compressed -L -H Expect: -H "X-API-Key: 49-99eab0ca16efde61012b3a535bab0edb" -F "problem_id=$1" -F "solution_spec=@answers/$1.dat" "http://2016sv.icfpcontest.org/api/solution/submit" > responses/$1.json
- cat responses/$1.json
+    curl --compressed -L -H Expect: -H "X-API-Key: 49-99eab0ca16efde61012b3a535bab0edb" -F "problem_id=$1" -F "solution_spec=@answers/$1.dat" "http://2016sv.icfpcontest.org/api/solution/submit" > responses/$1.json
+    if [ ! -s responses/$1.json ]; then
+        echo "Removing empty result"
+        rm responses/$1.json
+    else
+        cat responses/$1.json
+    fi
 else
- echo "$0 <qno>"
- echo " OR "
- echo "$0 <qno> <org-qno>"
+    echo "$0 <qno>"
+    echo " OR "
+    echo "$0 <qno> <org-qno>"
 fi
