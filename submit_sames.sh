dc888a2180a528b49610fdf5dfa2f5c4b11e80fa
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-08 01:26:59 +0900

    fix script.

--- submit_sames.sh
+++ submit_sames.sh
@@ -3,5 +3,8 @@
 # update clear problems
 ./update_clearlist.sh
 
+sleep=$1
+[ x"$sleep" != x ] || sleep=3600
+
 # attack for same problems
-fdupes -r problems/ | ./submit-same-answers "$1"
+fdupes -r problems/ | ./submit-same-answers $sleep
