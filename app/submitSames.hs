c4293285c008b61d865d78b2c28604e9ebbcd185
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 16:59:56 +0900

    fix.

--- app/submitSames.hs
+++ app/submitSames.hs
@@ -1,6 +1,8 @@
 
+import Control.Applicative
 import Control.Monad
 import Control.Concurrent (threadDelay)
+import Data.Bool
 import Data.Maybe
 import Data.List (stripPrefix, isSuffixOf, sort)
 import Data.List.Split (splitOn)
@@ -29,7 +31,8 @@ genDupesMap =
   . mapMaybe (uncons . sort . mapMaybe takeProblemNum)
   . splitOn [""] . lines
 
--- solutionFile n = "answers" </> show n <.> "dat"
+solutionFile :: Int -> FilePath
+solutionFile n = "answers" </> show n <.> "dat"
 
 problemFile :: Int -> FilePath
 problemFile n = "problems" </> show n <.> "dat"
@@ -43,12 +46,18 @@ runCommand cmd args = do
 
 submitCopyInterval :: Maybe Int -> Int -> Int -> IO ()
 submitCopyInterval sleep orig dest = do
-  let origFn = problemFile orig
-  origE <- doesFileExist origFn
-  if origE
-    then do runCommand "./submit_solution.sh" [show dest, show orig]
-            threadDelay $ maybe (3600 * 1000) (* 1000) sleep
-    else    putStrLn $ "original file not found: " ++ origFn
+  let probFn = problemFile orig
+      solFn  = solutionFile orig
+
+  e1  <-  bool (Just $ "original problem file not found: " ++ probFn) Nothing <$> doesFileExist probFn
+  e2  <-  bool (Just $ "original solution file not found: " ++ solFn) Nothing <$> doesFileExist solFn
+
+  case (e1 <|> e2) of
+    Nothing  -> do
+      runCommand "./submit_solution.sh" [show dest, show orig]
+      threadDelay $ maybe (3600 * 1000) (* 1000) sleep
+    Just err ->
+      putStrLn err
 
 submitCopies :: Maybe Int -> (Int, [Int]) -> IO ()
 submitCopies sleep (orig, cs) = mapM_ (submitCopyInterval sleep orig) cs
