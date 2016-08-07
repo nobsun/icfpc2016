13d01ede317243ee113d3a5f8cf57e82daba08c2
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 17:40:22 +0900

    add definition to get response-file path

--- src/File.hs
+++ src/File.hs
@@ -8,3 +8,6 @@ solutionFile n = "answers" </> show n <.> "dat"
 
 problemFile :: Int -> FilePath
 problemFile n = "problems" </> show n <.> "dat"
+
+responseFile :: Int -> FilePath
+responseFile n = "responses" </> show n <.> "json"
