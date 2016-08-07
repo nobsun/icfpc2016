5e5435c6e91e61b966a50cf137dd274b20ec919e
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 16:09:47 +0900

    add timestamp to loop.sh.

--- loop.sh
+++ loop.sh
@@ -2,9 +2,15 @@
 
 bsleep=$1
 
-[ x"$bsleep" != x ] || bsleep=4
+[ x"$bsleep" != x ] || bsleep=3.6
+
+date +"TIMESTAMP S: %Y-%m-%d %H:%M:%S"
 
 ./update_clearlist.sh
-./download.sh $bsleep
-./submit_sames.sh
+date +"TIMESTAMP 1: %Y-%m-%d %H:%M:%S"
 
+./download.sh $bsleep
+date +"TIMESTAMP 2: %Y-%m-%d %H:%M:%S"
+
+./submit_sames.sh
+date +"TIMESTAMP E: %Y-%m-%d %H:%M:%S"
