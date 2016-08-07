1b7a5ae4f0625e7476776c404f4da030d8afae7e
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 17:31:49 +0900

    add foolproof

--- src/Problem.hs
+++ src/Problem.hs
@@ -11,6 +11,7 @@ import Text.ParserCombinators.ReadP
 import Polygon
 import Segment
 import Vertex
+import File (problemFile, solutionFile)
 
 data Problem = Problem
   { nPolygon :: Int
@@ -72,16 +73,19 @@ loadProblem n = do
 
 genSimpleAnswer :: Int -> IO ()
 genSimpleAnswer n = do
-  b <- doesFileExist $ "problems" </> show n <.> "dat"
-  if b
-    then do
-    p <- loadProblem n
-    let vs = concatMap (map snd.pvertice.snd) $ polygons p
-        (dx, dy) = (minimum $ map xcoord vs, minimum $ map ycoord vs)
-        vs' = map (mv (dx, dy)) [(0,0), (1,0), (1,1), (0,1)]
-        ans = ["4", "0,0", "1,0", "1,1", "0,1", "1", "4 0 1 2 3"] ++ map showT vs'
-    writeFile ("answers/"++show n++".dat") $ unlines ans
-    else return ()
+  solFound  <-  doesFileExist $ solutionFile n
+  b         <-  doesFileExist $ problemFile n
+  if solFound
+    then putStrLn $ "solution file already exists: " ++ solutionFile n
+    else if b
+         then do
+           p <- loadProblem n
+           let vs = concatMap (map snd.pvertice.snd) $ polygons p
+               (dx, dy) = (minimum $ map xcoord vs, minimum $ map ycoord vs)
+               vs' = map (mv (dx, dy)) [(0,0), (1,0), (1,1), (0,1)]
+               ans = ["4", "0,0", "1,0", "1,1", "0,1", "1", "4 0 1 2 3"] ++ map showT vs'
+           writeFile ("answers/"++show n++".dat") $ unlines ans
+         else putStrLn $ "problem file not found: " ++ problemFile n
   where
     mv (dx, dy) (x, y) = (x+dx, y+dy)
     showR r = show (numerator r) ++ "/" ++ show (denominator r)
@@ -138,4 +142,3 @@ combine (Just xs) (Just ys) =
 
 cyclic :: [Segment] -> Bool
 cyclic = uncurry (==) . (fst . head &&& snd . last)
-
