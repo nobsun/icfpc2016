6d49ab21ac86aca94523c569e455456fc5557c3d
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 20:00:03 +0900

    add batch-submit-simples.

---
+++ app/batchSubmitSimples.hs
@@ -0,0 +1,23 @@
+
+import Control.Concurrent (threadDelay)
+import System.IO.Error (tryIOError)
+import System.Directory (doesFileExist)
+
+import File (responseFile)
+import Problem (genSimpleAnswer)
+import Command (runCommand)
+import ProblemDupes (genDupesMap)
+
+
+submitSimpleFirst :: Int -> IO ()
+submitSimpleFirst n = do
+  found  <- doesFileExist (responseFile n)
+  if found
+    then putStrLn $ "response already exists: " ++ responseFile n
+    else do
+    genSimpleAnswer n
+    runCommand "./submit_solution.sh" [show n]
+    threadDelay $ 3600 * 1000
+
+main :: IO ()
+main = mapM_ (tryIOError . submitSimpleFirst) . map fst . genDupesMap =<< getContents
