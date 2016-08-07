f75ef070b5d87a521535241ab31a89c7d2880656
Kei Hibino <ex8k.hibino@gmail.com>
2016-08-07 23:51:06 +0900

    check cleared result in submitSame.

--- app/submitSames.hs
+++ app/submitSames.hs
@@ -7,9 +7,11 @@ import Text.Read (readMaybe)
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
-import File (solutionFile, problemFile)
+import File (solutionFile, problemFile, responseFile)
 import Command (runCommand)
 import ProblemDupes (genDupesMap)
+import REST.Response (SolutionSubmission (..))
+import REST.File (loadSolutionSubmission)
 
 
 submitCopyInterval :: Maybe Int -> Int -> Int -> IO ()
@@ -20,12 +22,22 @@ submitCopyInterval sleep orig dest = do
   e1  <-  bool (Just $ "original problem file not found: " ++ probFn) Nothing <$> doesFileExist probFn
   e2  <-  bool (Just $ "original solution file not found: " ++ solFn) Nothing <$> doesFileExist solFn
 
-  case (e1 <|> e2) of
+  let respFn = responseFile dest
+      cleared ss
+        | abs (1.0 - solutionSubmission_resemblance ss) < 0.000001  =  Just $ "have cleared result: " ++ respFn
+        | otherwise                                                 =  Nothing
+
+  eResp <- doesFileExist respFn
+  clear <- if eResp
+           then (cleared =<<) <$> loadSolutionSubmission dest
+           else return Nothing
+
+  case (e1 <|> e2 <|> clear) of
     Nothing  -> do
       runCommand "./submit_solution.sh" [show dest, show orig]
       threadDelay $ maybe (3600 * 1000) (* 1000) sleep
-    Just err ->
-      putStrLn err
+    Just msg ->
+      putStrLn msg
 
 submitCopies :: Maybe Int -> (Int, [Int]) -> IO ()
 submitCopies sleep (orig, cs) = mapM_ (submitCopyInterval sleep orig) cs
