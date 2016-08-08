38864691522aaa0bbacb76a40e0b5c3e0cc10256
cutsea110 <cutsea110@gmail.com>
2016-08-08 15:32:41 +0900

    [FIX] distance' is not usable for this case
    We can use distance' for only segment'

--- src/Problem.hs
+++ src/Problem.hs
@@ -148,11 +148,15 @@ convex xs =
   where
     vs' = vs ++ vs'
     vs = map seg2vec xs
-    seg2vec :: Segment -> (Rational, Rational)
-    seg2vec (Vertex x1 y1, Vertex x2 y2) = (x2 - x1, y2 -y1)
+
+seg2vec :: Segment -> (Rational, Rational)
+seg2vec (Vertex x1 y1, Vertex x2 y2) = (x2 - x1, y2 -y1)
 
 crossProduct :: Num a => ((a, a), (a, a)) -> a
 crossProduct ((ax, ay), (bx, by)) = ax * by - ay * bx
+
+innerProduct :: Num a => ((a, a), (a, a)) -> a
+innerProduct ((ax, ay), (bx, by)) = ax * bx + ay * by
 
 volume' :: [Segment] -> Rational
 volume' = volume . map fst
@@ -168,8 +172,6 @@ volume (x:xs) = let v = foldr (+) 0 (map vol trigons)
     xs' = xs ++ xs'
     trigons :: [(Vertex, Vertex, Vertex)]
     trigons = take (length xs - 1) $ map (\(y,z) -> (x, y, z)) $ zip xs' (tail xs')
-    seg2vec :: Segment -> (Rational, Rational)
-    seg2vec (Vertex x1 y1, Vertex x2 y2) = (x2 - x1, y2 -y1)
 
 intersected' :: [Segment] -> Bool
 intersected' [] = False
@@ -186,9 +188,15 @@ intersected ((Vertex ax ay, Vertex bx by), (Vertex cx cy, Vertex dx dy)) =
     tb = cx_dx * by_cy + cy_dy * cx_bx
     tc = ax_bx * cy_ay + ay_by * ax_cx
     td = ax_bx * dy_ay + ay_by * ax_dx
+
 online :: Vertex -> Segment -> Bool
-online v (v1, v2) = v /= v1 && v /= v2 &&
-  distance' (v, v1) + distance' (v, v2) == (distance' (v1, v2) :: Rational)
+online v@(Vertex x y) (v1@(Vertex x1 y1), v2@(Vertex x2 y2)) =
+  notChained && notIntersected && inRange
+  where
+    (va, vb) = ((x1 - x, y1 - y), (x2 - x, y2 - y))
+    notChained = v /= v1 && v /= v2
+    notIntersected = crossProduct (va, vb) == 0
+    inRange = innerProduct (va, vb) < 0
 
 split :: Segment -> [Vertex] -> [Segment]
 split s@(v1, v2) vs = (zip <*> tail) orderedOnlineVertexes
@@ -197,6 +205,6 @@ split s@(v1, v2) vs = (zip <*> tail) orderedOnlineVertexes
     orderedOnlineVertexes = sortBy (compare `on` (sqDistance.(Vertex 0 0,))) (v1:v2:onlineVertexes)
 
 splitSegments :: [Segment] -> [Segment]
-splitSegments xs = concatMap (\x -> split x vs) xs
+splitSegments xs = concatMap (\x -> x `split` vs) xs
   where
     vs = foldr (\(v1,v2) r -> v1:v2:r) [] xs
